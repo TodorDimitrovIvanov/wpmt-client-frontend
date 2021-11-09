9f6975f67c4054b58cc2fed961f319cb67de24de
Ivo <73983660+IvoStanchev@users.noreply.github.com>
2021-11-09 11:01:42 +0200

    Added automatic start for the Python server.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,26 +1,29 @@
 import {
-  combineReducers,
-  applyMiddleware,
-  createStore,
-  PreloadedState,
-} from "redux";
-import thunkMiddleware from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+	combineReducers,
+	applyMiddleware,
+	createStore,
+	PreloadedState,
+	compose,
+} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-import { authReducer } from "./auth/reducers";
+import { authReducer } from './auth/reducers';
+
+//const composeEnchancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
-  auth: authReducer,
+	auth: authReducer,
 });
 
 export type AppState = ReturnType<typeof rootReducer>;
 
 export const configureStore = (initialState?: PreloadedState<AppState>) => {
-  return createStore(
-    rootReducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(thunkMiddleware))
-  );
+	return createStore(
+		rootReducer,
+		initialState,
+		composeWithDevTools(applyMiddleware(thunkMiddleware)),
+	);
 };
 
 export const store = configureStore();
