f6ad706627496bf8d70404a2f6a68fa83e6c996b
Ivo Stanchev <ivo.stanchev@siteground.com>
2022-07-22 11:26:14 +0300

    feat: [DEV-1] Create new project, implement SaSS, Redux, Typescript. Apply initial styling

---
+++ src/components/Layout.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react';
+import './layout.scss'
+
+const LayoutWPMT:FC = () => {
+
+    return (
+<div className='wpmt-main'>
+    <header>
+        <div className='wpmt-navbar-background'>
+            <ul className='wpmt-navbar-items'>
+                <li>
+                    <a>Dashboard</a>
+                </li>
+                <li>
+                    <a>Dashboard</a>
+                </li>
+                <li>
+                    <a>Dashboard</a>
+                </li>
+                <li>
+                    <a>Dashboard</a>
+                </li>
+                <li>
+                    <a>Dashboard</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+    {/*<main>*/}
+    {/*<div className='wpmt-main-background'>*/}
+
+    {/*</div>*/}
+    {/*</main>*/}
+</div>
+    )
+}
+
+export default LayoutWPMT;
--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,21 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import './index.scss';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={'/'} element={<App/>}/>
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
