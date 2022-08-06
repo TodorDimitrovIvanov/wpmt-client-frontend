69f6d8635f960a0596e84d83d1d13de155600df4
Ivo Stanchev <ivo.stanchev@siteground.com>
2022-08-06 14:48:00 +0300

    feat: [DEV-1] Code refactor, add Plugins, themes, Ads Performance, Troubleshooting, Actions sections

--- src/pages/Wordpress/Wordpress.tsx
+++ src/pages/Wordpress/Wordpress.tsx
@@ -1,70 +1,74 @@
 import LayoutWPMT from "../../components/Layout/Layout";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchPlugins, populate, selectPlugins } from "../../slices/wordpressSlice";
-import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { fetchPlugins, fetchTheme, selectPlugins, selectThemes } from "../../slices/wordpressSlice";
+import { useMemo } from "react";
 import './wordpress.scss';
-import wplogo from '../../assets/iconWordPress.svg';
+import wordPressSVG from '../../assets/WordPressLogo.svg';
+import pluginSVG from '../../assets/Plugins.svg'
+import themeSVG from '../../assets/Themes.svg'
+import adsSVG from '../../assets/Ads.svg'
+import troubleshootSVG from '../../assets/Troubleshoot.svg'
+import actionsSVG from '../../assets/Actions.svg'
+import MenuItem from "./MenuItem";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
 
 
 const Wordpress = () => {
 
-    const dispatch = useDispatch();
+    const dispatch = useAppDispatch();
 
-// @ts-ignore
-    const requestStatus: any = useSelector(state => state.wordpress.status);
-    const plugins = useSelector(selectPlugins);
+    const requestStatus: any = useAppSelector(state => state.wordpress.status);
+    const plugins = useAppSelector(selectPlugins);
+    const themes = useAppSelector(selectThemes)
     {/*{plugins.map((plugin: any) => <li key={plugin.textdomain} >{plugin.name}</li>)}*/
     }
 
-    useEffect(() => {
+    useMemo(() => {
         if (requestStatus === 'idle') {
-            // @ts-ignore
             dispatch(fetchPlugins());
+            dispatch(fetchTheme())
         }
     }, [ requestStatus, dispatch ]);
+
+    const activePlugins = plugins.reduce((sum: number, plugin: any) => {
+        if(plugin.status === 'active') {
+            sum++
+        }
+        return sum
+    }, 0)
+
+    const activeTheme = themes.filter((theme: { status: string; }) => theme.status === 'active')[0]?.stylesheet || 'Loading!'
+    console.log(activeTheme);
 
     return (
         <LayoutWPMT>
             <div className="wpmt-menu wpmt-wordpress-core__container">
-                <div className="wpmt-menu wpmt-wordpress_core__title">
-                    <img alt="WordPress icon" src={wplogo}/>
-                    <p className="wpmt-wordpress_core__text">Core</p>
-                </div>
-                <div className="wpmt-menu-item wpmt-wordpress_core__current-version">
-                    <div className="wpmt-wordpress_core__menu-item-text">
-                        <p>Current Version</p>
-                    </div>
-                    <div className="wpmt-wordpress_core__menu-item-information">
-                        <p>6.0</p>
-                    </div>
-                </div>
-                <div className="wpmt-menu-item wpmt-wordpress_core__latest-version">
-                    <div className="wpmt-wordpress_core__menu-item-text">
-                        <p>Latest Version</p>
-                    </div>
-                    <div className="wpmt-wordpress_core__menu-item-information">
-                        <p>6.0</p>
-                    </div>
-                </div>
-                <div className="wpmt-menu-item wpmt-wordpress_core__size">
-                    <div className="wpmt-wordpress_core__menu-item-text">
-                        <p>Website Size</p>
-                    </div>
-                    <div className="wpmt-wordpress_core__menu-item-information">
-                        <p>352 MB</p>
-                    </div>
-                </div>
-                <div className="wpmt-menu-item wpmt-wordpress_core__file-size">
-                    <div className="wpmt-wordpress_core__menu-item-text">
-                        <p>File Count</p>
-                    </div>
-                    <div className="wpmt-wordpress_core__menu-item-information">
-                        <p>32102 files</p>
-                    </div>
-                </div>
+                <MenuItem name="Core" iconAlt="WordPress Logo"  type="title" icon={wordPressSVG}/>
+                <MenuItem type="item" name="Current Version" value="6.0"/>
+                <MenuItem type="item" name="Latest Version" value="6.5"/>
+                <MenuItem type="item" name="Website Size" value="352 MB"/>
+                <MenuItem type="item" name="File Count" value="32102 files"/>
                 <button className="wpmt-menu wpmt-wordpress__core-button">Update</button>
-            </div>
 
+                <MenuItem name="Plugins" iconAlt="Puzzle piece" type="title" icon={pluginSVG}/>
+                <MenuItem type="item" name="Installed Plugins" value={plugins.length}/>
+                <MenuItem type="item" name="Enabled Plugins" value={activePlugins}/>
+
+                <MenuItem name="Themes" iconAlt="Brush" type="title" icon={themeSVG}/>
+                <MenuItem type="item" name="Installed themes" value={themes.length}/>
+                <MenuItem type="item" name="Active theme" value={activeTheme}/>
+
+                <MenuItem name="Ads" iconAlt="Cone-shaped Loudspeaker" type="title" icon={adsSVG}/>
+
+                <MenuItem name="Performance" iconAlt="WordPress logo" type="title" icon={wordPressSVG}/>
+                <MenuItem type="item" name="Google PageSpeed Score" value="29"/>
+                <MenuItem type="item" name="GTMetrix Score" value="21"/>
+
+                <MenuItem name="Troubleshooting" iconAlt="WordPress logo" type="title" icon={troubleshootSVG}/>
+
+                <MenuItem name="Actions" iconAlt="WordPress logo" type="title" icon={actionsSVG}/>
+
+            </div>
         </LayoutWPMT>
     );
 };
--- src/slices/wordpressSlice.ts
+++ src/slices/wordpressSlice.ts
@@ -3,6 +3,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 const token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvd29yZHByZXNzLml2b3N0YW5jaGV2LmNvbSIsImlhdCI6MTY1OTcwNzgxNCwibmJmIjoxNjU5NzA3ODE0LCJleHAiOjE2NjAzMTI2MTQsImRhdGEiOnsidXNlciI6eyJpZCI6IjIifX19.QJxk06Mg91XZlf7R7d7kGzjHENtJQn1H2lzSrqrisC4';
 const host = 'https://wordpress.ivostanchev.com';
 const pluginEndpoint = '/wp-json/wp/v2/plugins';
+const themeEndpoint = '/wp-json/wp/v2/themes';
 
 export const fetchPlugins = createAsyncThunk('/plugins/fetchPlugins', async () => {
     const response = await fetch(host + pluginEndpoint, {
@@ -17,10 +18,25 @@ export const fetchPlugins = createAsyncThunk('/plugins/fetchPlugins', async () =
     return response.json();
 });
 
+export const fetchTheme = createAsyncThunk('/plugins/fetchTheme', async () => {
+    const response = await fetch(host + themeEndpoint, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': token
+        }
+    });
+
+    return response.json();
+});
+
+
 export const wordpressSlice = createSlice({
     name: 'wordpress',
     initialState: {
-        data: [],
+        themes: [],
+        plugins: [],
         status: 'idle',
         error: ''
     }, reducers: {
@@ -33,15 +49,26 @@ export const wordpressSlice = createSlice({
                 state.status = 'loading';
             })
             .addCase(fetchPlugins.fulfilled, (state, action) => {
-                state.data = state.data.concat(action.payload);
+                state.plugins = state.plugins.concat(action.payload);
             })
             .addCase(fetchPlugins.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = 'An error has been encountered'
+            } )
+            .addCase(fetchTheme.pending, (state, action) => {
+                state.status = 'loading';
+            })
+            .addCase(fetchTheme.fulfilled, (state, action) => {
+                state.themes = state.themes.concat(action.payload);
+            })
+            .addCase(fetchTheme.rejected, (state, action) => {
                 state.status = 'failed'
                 state.error = 'An error has been encountered'
             } )
     }
 });
 
-export const selectPlugins = (state: { wordpress: { data: any; }; }) => state.wordpress.data
+export const selectPlugins = (state: { wordpress: { plugins: any; }; }) => state.wordpress.plugins;
+export const selectThemes = (state: { wordpress: { themes: any; }; }) => state.wordpress.themes;
 export const { populate } = wordpressSlice.actions;
 export default wordpressSlice.reducer;
